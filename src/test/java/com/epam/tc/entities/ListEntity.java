e5a628778b062a4b685db6810298d17a6667c4ca
iurii solomentsev <emirateskos@gmail.com>
2023-02-08 15:50:09 +0400

    fix

--- src/test/java/com/epam/tc/entities/ListEntity.java
+++ src/test/java/com/epam/tc/entities/ListEntity.java
@@ -1,7 +1,9 @@
 package com.epam.tc.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
 
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ListEntity(String id, String name) {
 }
